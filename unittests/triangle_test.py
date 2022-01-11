8db1019297f7cb8461e589119f629147dcb6914a
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2022-01-11 22:33:49 +0100

    Add placeholder unittest file for triangle class

---
+++ unittests/triangle_test.py
@@ -0,0 +1,49 @@
+"""
+Author: Kamil Niklasiński
+File: triangle_test.py
+Purpose: Unit tests for the Triangle class.
+"""
+
+from unittest import TestCase
+
+#  from shapes.rectangle import Rectangle
+
+
+class TriangleTest(TestCase):
+    """
+    Defines a test case for the Rectangle class.
+    """
+
+    pass
+
+    #  def setUp(self):
+    #      """
+    #      Create a few test objects.
+    #      """
+    #      self.len7wid3 = Rectangle(7, 3)
+    #      self.len1wid6 = Rectangle(1, 6)
+    #      self.len5wid5 = Rectangle(5, 5)
+
+    #  def test_area(self):
+    #      """
+    #      Compare the test rectangle area computations to the actual values.
+    #      """
+    #      self.assertEqual(self.len7wid3.area(), 21)
+    #      self.assertEqual(self.len1wid6.area(), 6)
+    #      self.assertEqual(self.len5wid5.area(), 25)
+
+    #  def test_perimeter(self):
+    #      """
+    #      Compare the test rectangle perimeter computations to the actual val.
+    #      """
+    #      self.assertEqual(self.len7wid3.perimeter(), 20)
+    #      self.assertEqual(self.len1wid6.perimeter(), 14)
+    #      self.assertEqual(self.len5wid5.perimeter(), 20)
+
+    #  def test_is_square(self):
+    #      """
+    #      Confirm or deny if the rectangle is a square.
+    #      """
+    #      self.assertFalse(self.len7wid3.is_square())
+    #      self.assertFalse(self.len1wid6.is_square())
+    #      self.assertTrue(self.len5wid5.is_square())
