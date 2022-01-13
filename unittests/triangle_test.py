1e763f2e66b6ecb38a31a2a66876070650faee1f
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2022-01-13 21:37:49 +0100

    Add unit tests (using built-in unittest module)

--- unittests/triangle_test.py
+++ unittests/triangle_test.py
@@ -6,44 +6,36 @@ Purpose: Unit tests for the Triangle class.
 
 from unittest import TestCase
 
-#  from shapes.rectangle import Rectangle
+from shapes.triangle import Triangle
 
 
 class TriangleTest(TestCase):
     """
-    Defines a test case for the Rectangle class.
+    Defines a test case for the Triangle class.
     """
 
-    pass
+    def setUp(self):
+        """
+        Create a few test objects.
+        """
+        self.leg1leg1leg1 = Triangle(1, 1, 1)  # triangle with valid legs
+        self.leg2leg1leg1 = Triangle(2, 1, 1)  # triangle with invalid legs
 
-    #  def setUp(self):
-    #      """
-    #      Create a few test objects.
-    #      """
-    #      self.len7wid3 = Rectangle(7, 3)
-    #      self.len1wid6 = Rectangle(1, 6)
-    #      self.len5wid5 = Rectangle(5, 5)
+    def test_area(self):
+        """
+        Compare the test triangle area computations to the actual values.
+        """
+        self.assertEqual(self.leg1leg1leg1.area(), 4.9)
 
-    #  def test_area(self):
-    #      """
-    #      Compare the test rectangle area computations to the actual values.
-    #      """
-    #      self.assertEqual(self.len7wid3.area(), 21)
-    #      self.assertEqual(self.len1wid6.area(), 6)
-    #      self.assertEqual(self.len5wid5.area(), 25)
+    def test_valid_legs(self):
+        """
+        Verify if triangle legs length is correctly validated.
+        """
+        self.assertFalse(self.leg2leg1leg1.valid_legs())
+        self.assertTrue(self.leg1leg1leg1.valid_legs())
 
-    #  def test_perimeter(self):
-    #      """
-    #      Compare the test rectangle perimeter computations to the actual val.
-    #      """
-    #      self.assertEqual(self.len7wid3.perimeter(), 20)
-    #      self.assertEqual(self.len1wid6.perimeter(), 14)
-    #      self.assertEqual(self.len5wid5.perimeter(), 20)
-
-    #  def test_is_square(self):
-    #      """
-    #      Confirm or deny if the rectangle is a square.
-    #      """
-    #      self.assertFalse(self.len7wid3.is_square())
-    #      self.assertFalse(self.len1wid6.is_square())
-    #      self.assertTrue(self.len5wid5.is_square())
+    def test_perimeter(self):
+        """
+        Compare the test triangle perimeter computations to the actual values.
+        """
+        self.assertEqual(self.leg1leg1leg1.perimeter(), 3)
